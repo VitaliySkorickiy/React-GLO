865b91a70cf137d61219d99028f476a6f6386558
zarias2020 <62381691+zarias2020@users.noreply.github.com>
2021-06-13 21:00:31 +0300

    разбил на компоненты

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="wrapper">
+        <div className="header">
+          <a href="tel:888">
+            <div className="header-logo"></div>
+          </a>
+          <a href="tel:888" className="header-phone"></a>
+          <div className="header-phonelink"><a href="tel:7(962)556-1234">+7(962)556-1234</a></div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
